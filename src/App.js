ae8254eb853781688d1488f0c5443c44953feef1
Chheang.chhea <chheangchhea008@gmail.com>
2023-04-23 10:14:54 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
+import ProductsList from './components/ProductsList';
+import Placeholder from 'react-bootstrap/Placeholder';
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='all-products'>
+    <ProductsList/>
     </div>
+    </>
   );
 }
 
